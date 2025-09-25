7194012fbc500a80461d00f04c7402a7f7ba63b0
Salim Abukar Ahmed <116594165+Saliim242@users.noreply.github.com>
2025-09-25 21:28:31 +0300

    feat(auth): add auth router to server
    Integrate authentication routes by mounting the authRouter at '/api/auth' endpoint. This enables handling of auth-related requests separately from other routes.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRouter from "./routers/auth.router.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+
+// Auth
+
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
